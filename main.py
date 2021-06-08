8c388e8cbe1aa573d62eceae0df6597e0dfe6722
JM <ja.morgan1@outlook.com>
2021-06-08 21:58:28 +1000

    make script runnable

--- main.py
+++ main.py
@@ -3,30 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.melbournewater.com.au/water-data-and-education/water-facts-and-history/why-melbournes-water-tastes-great/testing-water"
-headers = {'Accept-Language': 'en-US,en;q=0.5'}
 
-r = requests.get(url, headers)
-soup = BeautifulSoup(r.content, 'html.parser')
+def download(target_url):
+	headers = {'Accept-Language': 'en-US,en;q=0.5'}
 
-html_table = str(soup.find_all('table'))[1:].encode('ascii', 'ignore')
-data = pd.read_html(html_table)[0]
-data = data[['Parameter/ source', 'Silvan', 'Winneke']].set_index('Parameter/ source')
-data = data.filter(items=[
-	'pH (units)',
-	'Calcium',
-	'Magnesium',
-	'Sodium',
-	'Total alkalinity as CaCO3',
-	'Hardness',
-	'Zinc'
-], axis=0)
-# data[['Silvan Min', 'Silvan Max']] = data.Silvan.str.split('-', expand=True)
-silvan = data.Silvan.str.split('-', expand=True).replace({'<0.001': '0'}, regex=True).astype('float')
-winneke = data.Winneke.str.split('-', expand=True).replace({'<0.001': '0'}, regex=True).astype('float')
-data['Silvan'] = silvan.mean(axis=1)
-data['Winneke'] = winneke.mean(axis=1)
+	r = requests.get(target_url, headers)
+	soup = BeautifulSoup(r.content, 'html.parser')
 
-data = data.T
-data['Bicarbonate'] = data['Total alkalinity as CaCO3'] * 1.22
+	html_table = str(soup.find_all('table'))[1:].encode('ascii', 'ignore')
+	return html_table
 
-data.loc['mixture_5050'] = data.mean()
+def process_table(html_table):
+	data = pd.read_html(html_table)[0]
+	data = data[['Parameter/ source', 'Silvan', 'Winneke']].set_index('Parameter/ source')
+	data = data.filter(items=[
+		'pH (units)',
+		'Calcium',
+		'Magnesium',
+		'Sodium',
+		'Total alkalinity as CaCO3',
+		'Hardness',
+		'Zinc'
+	], axis=0)
+
+	silvan = data.Silvan.str.split('-', expand=True).replace({'<0.001': '0'}, regex=True).astype('float')
+	winneke = data.Winneke.str.split('-', expand=True).replace({'<0.001': '0'}, regex=True).astype('float')
+	data['Silvan'] = silvan.mean(axis=1)
+	data['Winneke'] = winneke.mean(axis=1)
+
+	data = data.T
+	data['Bicarbonate'] = data['Total alkalinity as CaCO3'] * 1.22
+
+	data.loc['mixture_5050'] = data.mean()
+
+	return data
+
+if __name__ == '__main__':
+	data = download(url)
+	result = process_table(data)
+	print(result)
